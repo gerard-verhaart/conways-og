637c3e981d1e9b6de2515c91279f75cdb5a06f8f
gerard-verhaart <gverhaart@gmail.com>
2023-02-25 17:59:24 +1300

    tried create board

--- createBoard.js
+++ createBoard.js
@@ -1,3 +1,10 @@
-function createBoard(size) {}
+function createBoard(size) {
+  // newArr = []
+  // for (let i = 0; i < size.length; i++) {
+  //   newArr[i]
+  //   for (let j = 0; j < size.length; j++) {
+  //   }
+  // }
+}
 
 module.exports = createBoard
