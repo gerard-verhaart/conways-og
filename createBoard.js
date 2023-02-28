9981b6184e8f6ad90c7a8eee14320286d3c2ac01
gerard-verhaart <gverhaart@gmail.com>
2023-02-28 17:34:22 +1300

    added parts

--- createBoard.js
+++ createBoard.js
@@ -3,9 +3,10 @@ function createBoard(size) {
   for (let i = 0; i < size; i++) {
     newBoard[i] = []
     for (let j = 0; j < size; j++) {
-      newBoard[i][j] = false
+      newBoard[i][j] = Math.random() < 0.4 ? true : false
     }
   }
+  // console.log(newBoard)
   return newBoard
 }
 
--- nextBoard.js
+++ nextBoard.js
@@ -1,6 +1,19 @@
 const nextCellState = require('./nextCellState')
 const countAliveNeighbours = require('./countAliveNeighbours')
 
-function nextBoard(currentBoard) {}
+function nextBoard(currentBoard) {
+  let nextBoard = []
+  for (let i = 0; i < currentBoard.length; i++) {
+    nextBoard[i] = []
+    for (let j = 0; j < currentBoard[0].length; j++) {
+      nextBoard[i][j] = nextCellState(
+        currentBoard[i][j],
+        countAliveNeighbours(i, j, currentBoard)
+      )
+    }
+  }
+  // console.log(nextBoard)
+  return nextBoard
+}
 
 module.exports = nextBoard
